e2c0ea93e226d7f06223463b3622b98b3e4420df
Alfrancis C. De Vera <alfrancidevera261@gmail.com>
2026-04-28 21:49:44 +0800

    added conditions to know if the integer inodd or even

--- SquareCube/square_even_cube_odd.py
+++ SquareCube/square_even_cube_odd.py
@@ -9,3 +9,8 @@ class IntegerProcessor:
         
         for line in file:
             number = int(line.strip())
+            
+            if number % 2 == 0:
+                even_results.append(number ** 2)  # square
+            else:
+                odd_results.append(number ** 3)   # cube
